962bd25e2c47ad0902fd1a611cec91fc6cc91d20
taylorrubbert <tayrubbert@hotmail.com>
2024-04-21 15:43:48 -0500

    fixed typo

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const initalCards = [
+const initialCards = [
   {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
@@ -25,4 +25,4 @@ const initalCards = [
   },
 ];
 
-console.log(initalCards);
+console.log(initialCards);
