a876a57b37291270669143a7f62b9c694c011e7a
taylorrubbert <tayrubbert@hotmail.com>
2024-04-21 15:40:56 -0500

    js initial add

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const initalCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Theodore Roosevelt National Park",
+    link: "https://unsplash.com/photos/green-grass-field-and-brown-mountains-during-daytime-oz0b8WpWRuY",
+  },
+  {
+    name: "Redwood National Forest",
+    link: "https://unsplash.com/photos/landscape-photography-of-forest-3tW2GR0KAd8",
+  },
+  {
+    name: "Acadia",
+    link: "https://unsplash.com/photos/green-trees-on-rocky-shore-during-daytime-QaIXVqqk3H0",
+  },
+  {
+    name: "Lake Clark",
+    link: "https://unsplash.com/photos/green-tent-on-rocky-shore-near-mountain-under-cloudy-sky-during-daytime-7SuSlm33MiY",
+  },
+  {
+    name: "Everglades",
+    link: "https://unsplash.com/photos/a-large-body-of-water-filled-with-lots-of-green-plants-iac26tSGcJ4",
+  },
+];
+
+console.log(initalCards);
